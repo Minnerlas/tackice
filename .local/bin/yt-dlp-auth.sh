77fcaa10fc473a1004dc20a17376544b09534c8f
Nikola <nikola@radojevic.rs>
2024-08-09 14:10:29 +0200

    yt-dlp-auth.sh

---
+++ .local/bin/yt-dlp-auth.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+machine() {
+	printf 'machine %s login %s password %s\n' "$1" "$2" "$3"
+}
+
+pw() {
+	pass show "$1" | head -n1
+}
+
+machine nebula nikilirikili@gmail.com "$(pw nebula.tv/nikilirikili@gmail.com)"
